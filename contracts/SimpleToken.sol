2612718552587158eff82202713b9ebe5fb7ea21
zeynep erdem <120062955+erdem4663@users.noreply.github.com>
2025-09-27 17:00:28 +0300

    Create SimpleToken.sol

---
+++ contracts/SimpleToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    string public name = "SimpleToken";
+    string public symbol = "STK";
+    uint256 public totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_to] += _amount;
+        return true;
+    }
+}
